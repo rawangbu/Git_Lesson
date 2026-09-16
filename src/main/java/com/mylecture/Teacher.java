2c1deccc9da2c5a93ef9eed4f181c733efd2af9d
rawangbu <rawang@bethlehem.edu>
2026-09-16 10:02:23 +0300

    teacher construct

--- src/main/java/com/mylecture/Teacher.java
+++ src/main/java/com/mylecture/Teacher.java
@@ -2,4 +2,12 @@ package com.mylecture;
 
 public class Teacher {
     private String name;
+
+    public Teacher(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
